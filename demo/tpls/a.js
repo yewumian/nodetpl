5f4161c243f7e6e88fad3e248267cf01afd3cb83
Pillys <pillys@163.com>
2015-12-01 22:56:48 +0800

    update

--- demo/tpls/a.js
+++ demo/tpls/a.js
@@ -57,35 +57,39 @@ for(var i=0; i<favor.length; i++){
 }
     _ += '<script>';
     _ += '(function(window, document, undefined){\n';
-    _ += '  var __module_id = "_main";\n';
-    _ += '  var __callback = function(nodetpl){\n';
+    _ += '  var __module_id = "'+ PATH + '_main";\n';
+    _ += '  var __callback = function(nodetpl, guid, dguid){\n';
     _ += '    var ROOT, $ROOT, SUBROOT, $SUBROOT, $TPLS, $DATA;\n';
-    _ += '    ROOT = document.getElementById("'+ guid +'");\n';
-    _ += '    SUBROOT = document.getElementById("'+ guid + dguid +'");\n';
+    _ += '    ROOT = document.getElementById(guid);\n';
+    _ += '    SUBROOT = document.getElementById(guid + dguid);\n';
     _ += '    $TPLS = nodetpl._tpls["'+ PATH +'"];\n';
-    _ += '    $DATA = nodetpl._data["'+ dguid +'"];\n';
+    _ += '    $DATA = nodetpl._data[dguid];\n';
     _ += '    try{\n';
-    _ += '      $ROOT = '+ N.options.vars.root.replace(/~/, guid) + ';\n';
-    _ += '      $SUBROOT = '+ N.options.vars.root.replace(/~/, guid + dguid) + ';\n';
+    _ += '      $ROOT = '+ N.options.vars.root.replace(/~/, '"+ guid + "') + ';\n';
+    _ += '      $SUBROOT = '+ N.options.vars.root.replace(/~/, '"+ guid + dguid + "') + ';\n';
     _ += '    } catch(e) { }\n';
     _ += 'console.log($ROOT);\n';
-    _ += '    delete nodetpl._data["'+ dguid +'"];\n';
+    _ += '    delete nodetpl._data[dguid];\n';
     _ += '  };\n';
-if (typeof define === 'function' && define.cmd && typeof seajs === 'object') {
-  // CMD seaJs
-    _ += '  define(__module_id, function(require, exports, module){\n';
-    _ += '    var nodetpl = require(\'nodetpl\');\n';
-    _ += '    __callback(nodetpl);\n';
-    _ += '  });\n';
-    _ += '  seajs.use(__module_id);\n';
-} else if (typeof define === 'function' && define.amd && typeof require === 'function') {
-  // AMD requireJs
-    _ += '  require(\'nodetpl\', function(nodetpl){\n';
-    _ += '    __callback(nodetpl);\n';
-    _ += '  });\n';
-} else {
-    _ += '__callback(window.nodetpl);\n';
-}
+    if (typeof define === 'function' && define.cmd && typeof seajs === 'object') {
+      // CMD seaJs
+      _ += '  define(__module_id, function(require, exports, module){\n';
+      _ += '    var nodetpl = require(\'nodetpl\');\n';
+      _ += '    return function(guid, dguid){\n';
+      _ += '      __callback(nodetpl, guid, dguid);\n';
+      _ += '    };\n';
+      _ += '  });\n';
+      _ += '  seajs.use(__module_id, function(fn){\n';
+      _ += '    fn && fn("'+ guid + '", "'+ dguid + '");\n';
+      _ += '  });\n';
+    } else if (typeof define === 'function' && define.amd && typeof require === 'function') {
+      // AMD requireJs
+      _ += '  require(\'nodetpl\', function(nodetpl){\n';
+      _ += '    __callback(nodetpl);\n';
+      _ += '  });\n';
+    } else {
+      _ += '__callback(window.nodetpl);\n';
+    }
     _ += '})(window, document);\n';
     _ += '</script>\n';
     $DATA && (N._data[dguid] = $DATA);
