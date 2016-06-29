fd650246bf04f033d57cb4fcfad7b091106217b2
Pillys <pillys@163.com>
2016-06-29 19:19:14 +0800

    ++

--- static/js/require-config.js
+++
@@ -1,9 +0,0 @@
-require.config({
-  baseUrl: '/static/js/',
-  paths: {
-    'es5-safe': 'lib/es5-sham.min',
-    'jquery': 'lib/jquery-1.10.2',
-    'vDialog': 'lib/vDialog.min',
-    'nodetpl': 'lib/nodetpl'
-  }
-});
--- static/js/view/cn/faq.js
+++
@@ -1,8 +0,0 @@
-define(function(require, exports, module) {
-  'use strict';
-
-  var $ = require('jquery');
-  $('section.faq-list dl dt a').on('click', function() {
-    $(this).closest('dl').toggleClass('active');
-  });
-});
